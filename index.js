23c41a9f84993a155c0d04c74dfbfcc874844494
Wesley Queiroz <wesley.coder@gmail.com>
2015-03-13 12:03:15 -0300

    Just remove the console.log
    We do not need the console.log spamming our output from gulp...

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ module.exports = function(options) {
   options = options || {};
   return es.map(function (file, cb) {
  	var html = file.contents.toString();
-		html2jade.convertHtml(html, options, function (err, jade) { 
-	    console.log(jade);
+	    html2jade.convertHtml(html, options, function (err, jade) { 
+	    //console.log(jade);
 	    file.contents = new Buffer( jade );
 	    file.path = gutil.replaceExtension(file.path, '.jade');
 	    cb(null,file);
